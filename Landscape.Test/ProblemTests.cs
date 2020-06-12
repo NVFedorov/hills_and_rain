75fb7251e89ba0ca9b746731e6b2d9f0e0fdb4b9
Nikita Fedorov <NFedorov@vubiquity.com>
2020-06-12 19:23:11 +0300

    solved

--- Landscape.Test/ProblemTests.cs
+++ Landscape.Test/ProblemTests.cs
@@ -5,33 +5,169 @@ namespace Landscape.Test
 {
     public class ProblemTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void PositiveTest()
+        public void Should_Return_Zero_For_Downhill_No_Pits()
         {
-            var landscape = new int[] { 5, 2, 3, 4, 5, 4, 1, 3, 1};
+            var landscape = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
             var result = Problem.Solve(landscape);
-            Assert.AreEqual(8, result);
+            Assert.AreEqual(0, result);
         }
 
         [Test]
-        public void WrongDataTest()
+        public void Should_Return_Zero_For_Uphill_No_Pits()
+        {
+            var landscape = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Downhill_With_Pits()
+        {
+            var landscape = new int[] { 9, 7, 8, 5, 6, 3, 4, 1, 2 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Uphill_With_Pits()
+        {
+            var landscape = new int[] { 2, 1, 4, 3, 6, 5, 8, 7, 9 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Down_Vallye_Up()
+        {
+            var landscape = new int[] { 9, 8, 7, 6, 6, 6, 7, 8, 9 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(15, result);
+        }
+
+        [Test]
+        public void Should_Return_Zero_For_Up_Down()
+        {
+            var landscape = new int[] { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Up_Down_Up_Down_Up()
+        {
+            var landscape = new int[] { 1, 2, 3, 2, 5, 1, 2, 3, 4 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Start_With_Uphill()
+        {
+            var landscape = new int[] { 1, 2, 3, 4, 5, 4, 1, 3, 1 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Complex_Pit()
+        {
+            var landscape = new int[] { 7, 2, 3, 4, 5, 4, 1, 3, 6 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(20, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Two_Complex_Pits()
+        {
+            var landscape = new int[] { 9, 5, 2, 4, 5, 10, 4, 8, 3, 21 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(35, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Two_Pits_With_Plato_Between()
+        {
+            var landscape = new int[] { 5, 1, 2, 1, 5, 5, 5, 5, 3, 21 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(13, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Three_Pits()
+        {
+            var landscape = new int[] { 5, 2, 3, 4, 3, 4, 1, 3, 1 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Three_Complex_Pits()
+        {
+            var landscape = new int[] { 10, 1, 5, 3, 9, 1, 3, 5, 3, 8, 3, 4, 5, 5, 4, 7, 1 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(52, result);
+        }
+
+        [Test]
+        public void Should_Return_Zero_When_Landscape_Length_Is_One()
+        {
+            var landscape = new int[1];
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Should_Return_Zero_When_Flat()
+        {
+            var landscape = new int[Problem.MaxLandscapeSize];
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Should_Throw_Exceptions_For_WrongData()
         {
             var landscape = new int[] { 12, 32, 0, -1 };
             var exc = Assert.Throws<ArgumentException>(() => Problem.Solve(landscape));
             Assert.That(exc.Message, Is.EqualTo($"The hill heights values must be between 0 and {Problem.MaxHeightValues}"));
 
-            landscape = new int[] { Problem.MaxHeightValues + 1 };
+            landscape = new int[] { 12, 32, -1, 1 };
             exc = Assert.Throws<ArgumentException>(() => Problem.Solve(landscape));
             Assert.That(exc.Message, Is.EqualTo($"The hill heights values must be between 0 and {Problem.MaxHeightValues}"));
 
-            landscape = new int[Problem.MaxLandscapeSize];
+            landscape = new int[] { 12, Problem.MaxHeightValues + 1, 0 };
+            exc = Assert.Throws<ArgumentException>(() => Problem.Solve(landscape));
+            Assert.That(exc.Message, Is.EqualTo($"The hill heights values must be between 0 and {Problem.MaxHeightValues}"));
+
+            landscape = new int[Problem.MaxLandscapeSize + 1];
             exc = Assert.Throws<ArgumentException>(() => Problem.Solve(landscape));
             Assert.That(exc.Message, Is.EqualTo($"The max landscape size is {Problem.MaxLandscapeSize}"));
+        }
+
+        // just in case
+        [Test]
+        public void Should_Solve_For_Long_Downhill()
+        {
+            var landscape = new int[] { 5, 2, 3, 4, 3, 2, 1, 4, 1 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_LongDownhill_2()
+        {
+            var landscape = new int[] { 5, 4, 3, 2, 1, 5, 4, 5, 6 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(11, result);
+        }
+
+        [Test]
+        public void Should_Solve_For_Uphill_With_Small_Pits()
+        {
+            var landscape = new int[] { 1, 3, 2, 4, 3, 5, 4, 3, 7 };
+            var result = Problem.Solve(landscape);
+            Assert.AreEqual(5, result);
         }
     }
 }
--- Landscape/Problem.cs
+++ Landscape/Problem.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Landscape
 {
@@ -20,15 +20,87 @@ namespace Landscape
         /// <returns>Area of the filled by water pits.</returns>
         public static int Solve(int[] heights)
         {
-            if (heights.Length >= MaxLandscapeSize)
-                throw new ArgumentException($"The max landscape size is {MaxLandscapeSize}"); 
-            
-            if (heights.Any(x => x < 0 || x > MaxHeightValues))
+            var timer = new System.Diagnostics.Stopwatch();
+            if (heights.Length > MaxLandscapeSize)
+                throw new ArgumentException($"The max landscape size is {MaxLandscapeSize}");
+
+            if (heights[^1] < 0 || heights[^1] > MaxHeightValues)
                 throw new ArgumentException($"The hill heights values must be between 0 and {MaxHeightValues}");
 
+            timer.Start();
+            var result = SolveProblem(heights);
+            timer.Stop();
+            System.Diagnostics.Debug.WriteLine($"Elapsed time: {timer.ElapsedMilliseconds}");
+            return result;
+        }
 
+        private static int SolveProblem(int[] heights)
+        {
+            var result = 0;
+            var hillStartEndPairs = new List<int[]>();
+            for (var i = 0; i < heights.Length - 1; i++)
+            {
+                if (heights[i] < 0 || heights[i] > MaxHeightValues)
+                    throw new ArgumentException($"The hill heights values must be between 0 and {MaxHeightValues}");
 
-            return 0;
+                if (heights[i] > heights[i + 1])
+                {
+                    var max = 0;
+                    var maxJ = -1;
+                    var push = false;
+                    for (var j = i + 1; j < heights.Length - 1; j++)
+                    {
+                        if (heights[j] < 0 || heights[j] > MaxHeightValues)
+                            throw new ArgumentException($"The hill heights values must be between 0 and {MaxHeightValues}");
+                        //iterations++;
+                        if (heights[j] < heights[j + 1])
+                        {
+                            push = true;
+                            if (heights[i] <= heights[j + 1])
+                            {
+                                maxJ = j + 1;
+                                break;
+                            }
+                            else if (heights[j + 1] > max)
+                            {
+                                maxJ = j + 1;
+                                max = heights[maxJ];
+                            }
+                        }
+                    }
+
+                    if (maxJ >= 0 && push)
+                    {
+                        var replaced = false;
+                        if (hillStartEndPairs.Count > 0)
+                        {
+                            if (hillStartEndPairs[^1][0] <= i && maxJ <= hillStartEndPairs[^1][1])
+                            {
+                                replaced = true;
+                            }
+                        }
+
+                        if (!replaced)
+                            hillStartEndPairs.Add(new int[] { i, maxJ });
+
+                        if (maxJ > i)
+                            i = maxJ - 1;
+                    }
+                }
+                
+            }
+
+            foreach(var pair in hillStartEndPairs)
+            {
+                var min = Math.Min(heights[pair[0]], heights[pair[1]]);
+                for (var i = pair[0]; i < pair[1]; i++)
+                {
+                    result += Math.Max(0, min - heights[i]);
+                    //iterations++;
+                }
+            }
+
+            return result;
         }
     }
 }
